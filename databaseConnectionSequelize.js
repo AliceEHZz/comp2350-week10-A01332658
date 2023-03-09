e91eb114c485dc847938d0b60814291dea9eb999
AliceEHZz <meiyellmya@gmail.com>
2023-03-09 15:34:53 -0800

    fixed qoddi connection

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,8 +1,8 @@
 const is_qoddi = process.env.IS_QODDI || false;
 const dbConfigQoddi =
-  "mysql://freedb_2350_main:E&Vj97Da7NS6%c8@sql.freedb.tech:3306/freedb_comp2350-week2-A01332658";
+  "mysql://freedb_2350_main:E%26Vj97Da7NS6%25c8@sql.freedb.tech:3306/freedb_comp2350-week2-A01332658";
 const dbConfigLocal =
-  "mysql://root:SQLJ?~3u%COMP2350zx7DxYeGm+@localhost/web_user";
+  "mysql://root:SQLJ%3F~3u%25COMP2350zx7DxYeGm%2B@localhost/web_user";
 if (is_qoddi) {
   var databaseConnectionString = dbConfigQoddi;
 } else {
--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,9 @@ const router = require("express").Router();
 // const dbModel = include('databaseAccessLayer');
 //const dbModel = include('staticData');
 const userModel = include("models/web_user");
+const crypto = require("crypto");
+const { v4: uuid } = require("uuid");
+const passwordPepper = "SeCretPeppa4MySal+";
 
 router.get("/", async (req, res) => {
   console.log("page hit");
@@ -37,6 +40,29 @@ router.get("/deleteUser", async (req, res) => {
         await deleteUser.destroy();
       }
     }
+    res.redirect("/");
+  } catch (ex) {
+    res.render("error", { message: "Error connecting to MySQL" });
+    console.log("Error connecting to MySQL");
+    console.log(ex);
+  }
+});
+
+router.post("/addUser", async (req, res) => {
+  try {
+    console.log("form submit");
+    const password_salt = crypto.createHash("sha512");
+    password_salt.update(uuid());
+    const password_hash = crypto.createHash("sha512");
+    password_hash.update(req.body.password + passwordPepper + password_salt);
+    let newUser = userModel.build({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password_salt: password_salt.digest("hex"),
+      password_hash: password_hash.digest("hex"),
+    });
+    await newUser.save();
     res.redirect("/");
   } catch (ex) {
     res.render("error", { message: "Error connecting to MySQL" });
