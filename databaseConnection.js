642e8c2467d8847d92e5a2f909c4896d184adb46
AliceEHZz <meiyellmya@gmail.com>
2023-03-09 14:43:35 -0800

    fix freedb connection

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,29 +1,27 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const is_qoddi = process.env.IS_QODDI || false;
 
 const dbConfigQoddi = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
-	multipleStatements: false
+  host: "sql.freedb.tech",
+  user: "freedb_main_2350",
+  password: "E&Vj97Da7NS6%c8",
+  database: "freedb_comp2350-week2-A01332658",
+  multipleStatements: false,
 };
 
 const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "database1",
-	multipleStatements: false
+  host: "localhost",
+  user: "root",
+  password: "Password",
+  database: "database1",
+  multipleStatements: false,
 };
 
 if (is_qoddi) {
-	var database = mysql.createPool(dbConfigQoddi);
-}
-else {
-	var database = mysql.createPool(dbConfigLocal);
+  var database = mysql.createPool(dbConfigQoddi);
+} else {
+  var database = mysql.createPool(dbConfigLocal);
 }
 
 module.exports = database;
-		
